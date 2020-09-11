fba7e9c644d5ffc9c0ac4d6c8034217de53bdb89
fenpatagnan <fen.patagnan.greenova.ph@outlook.com>
2020-09-11 16:43:37 +0800

    master: transfer ajax to custom validation js

--- Scripts/customValidation.js
+++ Scripts/customValidation.js
@@ -1,1 +1,24 @@
 ﻿
+$("#projectCode").keyup(function () {
+
+    let charCount = $('#projectCode').val().length;
+    
+if (charCount >= 5) {
+    runHTTPRequest()
+}
+})
+    
+function runHTTPRequest() {
+    $.post('@Url.Action("IsProjectAvail", "Projects")',
+        {
+            projectCode: $("#projectCode").val()
+        },
+        function (data) {
+            if (data == 0) {
+                $("#projectCodeAvailability").html("<small class=\"text-success\"><strong>Project is available.</strong></small>");
+            } else {
+                $("#projectCodeAvailability").html("<small class=\"text-danger\"><strong>Project is not available.</strong></small>");
+            }
+        })
+}
+    
